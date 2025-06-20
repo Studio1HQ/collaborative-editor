1193c1566435c5bb26e35864cbe537de7ffd2a21
timonwa <timonwaakintokun@gmail.com>
2025-06-20 15:46:13 +0100

    added pages router

---
+++ pages/editors/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
+import AuthProvider from "@/providers/AuthProvider";
+
+function MyComponent() {
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <ReactQuill theme="snow" value={value} onChange={setValue} />
+
+      <div className="mt-4">
+        <h2 className="text-lg font-semibold mb-2">Live Output:</h2>
+        <div className="p-2 border rounded bg-gray-50 min-h-[40px]">
+          <div dangerouslySetInnerHTML={{ __html: value }} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default function Page() {
+  return (
+    <AuthProvider>
+      <main className="container mx-auto p-4">
+        <h1 className="text-2xl font-semibold mb-4">Shared Document</h1>
+
+        <MyComponent />
+      </main>
+    </AuthProvider>
+  );
+}
