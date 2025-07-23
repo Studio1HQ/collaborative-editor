a8d8ef732f3b8dbe3109c26c2e23e403f151af4e
timonwa <timonwaakintokun@gmail.com>
2025-07-23 19:40:21 +0100

    feat: enhance RichTextEditor layout and add custom tooltip styling

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -69,28 +69,37 @@ const RichTextEditor = () => {
   }, [client]);
 
   return (
-    <div className="flex flex-col md:flex-row gap-8 mt-8 h-[calc(100vh-8rem)]">
-      <section className="flex-1 md:w-[50%] bg-white rounded-lg shadow-lg p-6 flex flex-col">
-        <h2 className="text-lg font-semibold mb-4 text-gray-700">Editor</h2>
-        <div className="flex-1 max-h-full overflow-auto">
-          <ReactQuill
-            theme="snow"
-            value={value}
-            onChange={setValue}
-            style={{ height: "100%" }}
-          />
-        </div>
-      </section>
-      <section className="flex-1  md:w-[50%] bg-white rounded-lg shadow-lg p-6 flex flex-col">
-        <h2 className="text-lg font-semibold mb-4 text-gray-700">Preview</h2>
-        <div className="flex-1 min-h-0 max-h-full overflow-auto max-w-none border-2 p-4 rounded-sm border-gray-200">
-          <div
-            className="editor-output"
-            dangerouslySetInnerHTML={{ __html: value }}
-          />
-        </div>
-      </section>
-    </div>
+    <>
+      <div className="flex flex-col md:flex-row gap-8 mt-8 h-[calc(100vh-8rem)]">
+        <section className="flex-1 md:w-[50%] bg-white rounded-lg shadow-lg p-6 flex flex-col">
+          <h2 className="text-lg font-semibold mb-4 text-gray-700">Editor</h2>
+          <div className="flex-1 max-h-full overflow-auto">
+            <ReactQuill
+              theme="snow"
+              value={value}
+              onChange={setValue}
+              style={{ height: "100%" }}
+            />
+          </div>
+        </section>
+        <section className="flex-1  md:w-[50%] bg-white rounded-lg shadow-lg p-6 flex flex-col">
+          <h2 className="text-lg font-semibold mb-4 text-gray-700">Preview</h2>
+          <div className="flex-1 min-h-0 max-h-full overflow-auto max-w-none border-2 p-4 rounded-sm border-gray-200">
+            <div
+              className="editor-output"
+              dangerouslySetInnerHTML={{ __html: value }}
+            />
+          </div>
+        </section>
+      </div>
+      <style>
+        {`
+          .ql-tooltip {
+            left: 0 !important;
+          }
+        `}
+      </style>
+    </>
   );
 };
 
